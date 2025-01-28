199191540d1b98f5f84f7749e2f425674d5dff9e
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2025-01-28 23:12:44 +0530

    Exploring the Get, Post,Use,delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,42 @@ const express=require("express");
 
 const app=express();
 
-app.get("/",(req,res)=>{
-    res.send("Namaste");
-});
+ 
+// app.use("/hello/2" ,(req,res)=>{
+//     res.send("Heelo ji I placed in Google");
+//  });
 
-app.get("/test" ,(req,res)=>{
+//This will only call the use /test
+app.use("/test" ,(req,res)=>{
    res.send("Heelo ji from server");
 });
+  
+//This will only get the GET call to /user
+app.get("/user",(req,res)=>{
+     res.send({firstname:"Deepanshu",
+        lastname:"Kohli"
+     })
+});
 
-app.get("/hello" ,(req,res)=>{
-    res.send("Heelo ji from Akshay Saini");
- });
+
+/// This will post the /user
+app.post("/user",(req,res)=>{
+    res.send("Data is successfully saved to data base");
+});
+
+//Making the delete CAll
+app.delete("/user",(req,res)=>{
+    res.send("Data of user deleted succesfully");
+})
+
+// app.use("/hello" ,(req,res)=>{
+//     res.send("Heelo ji from Akshay Saini");
+//  });
+ 
+// app.use("/",(req,res)=>{
+//     res.send("Namaste");
+// }); 
+
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening the port 3000..")
