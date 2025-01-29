db13c9aa2a68cbe7856aed93bd7729dff5767af1
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2025-01-29 15:55:23 +0530

    Multiple route Handlers

--- src/app.js
+++ src/app.js
@@ -3,32 +3,49 @@ const express=require("express");
 const app=express();
 
  
+
+//Here ac and abc will work
+app.get("/user/:userid/:Password/:name",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname:"lucky",
+        lastname:"Yadav"
+    })
+})
+
+//This is regex if you write like {car ,aihd,ajkbk} a should be included
+app.get(/a/,(req,res)=>{
+    res.send({firstname:"lucky",
+        lastname:"Yadav"
+    })
+})
+
+
 // app.use("/hello/2" ,(req,res)=>{
 //     res.send("Heelo ji I placed in Google");
 //  });
 
 //This will only call the use /test
-app.use("/test" ,(req,res)=>{
-   res.send("Heelo ji from server");
-});
+// app.use("/test" ,(req,res)=>{
+//    res.send("Heelo ji from server");
+// });
   
 //This will only get the GET call to /user
-app.get("/user",(req,res)=>{
-     res.send({firstname:"Deepanshu",
-        lastname:"Kohli"
-     })
-});
+// app.get("/user",(req,res)=>{
+//      res.send({firstname:"Deepanshu",
+//         lastname:"Kohli"
+//      })
+// });
 
 
 /// This will post the /user
-app.post("/user",(req,res)=>{
-    res.send("Data is successfully saved to data base");
-});
+// app.post("/user",(req,res)=>{
+//     res.send("Data is successfully saved to data base");
+// });
 
 //Making the delete CAll
-app.delete("/user",(req,res)=>{
-    res.send("Data of user deleted succesfully");
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("Data of user deleted succesfully");
+// })
 
 // app.use("/hello" ,(req,res)=>{
 //     res.send("Heelo ji from Akshay Saini");
