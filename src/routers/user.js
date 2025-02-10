99e9bccc124cf6fd57872247c1508fd743cb3925
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2025-02-10 17:17:50 +0530

    created feed api and pagination skip and limit

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,8 +4,8 @@ const UserRouter=express.Router();
 const {userAuth}=require("../middlewares/auth");
 const ConnectionRequest=require("../models/ConnectionRequest");
 
-const USER_SAFE="firstName lastName age gender skills"
-
+const USER_SAFE="firstName lastName age gender skills";
+const User =require("../models/user");
 
 UserRouter.get("/user/request/recieved",userAuth, async (req,res)=>{
 
@@ -90,6 +90,65 @@ UserRouter.get("/user/connection",userAuth, async (req,res)=>{
 
     }
     
+});
+
+
+
+UserRouter.get("/feed",userAuth, async (req,res)=>{
+
+
+    try{
+        //User Should see all the User cards except
+        //0. His Own card
+        //1. His Connection
+        //2. Ignored people
+        //3. already sent connection
+
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit) || 10;
+        limit=limit >50 ? (50): (limit);
+
+
+        const skip=(page-1)*limit;
+
+        const loggedIn=req.UserInfo;
+
+        const Connection =await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedIn._id},
+                {ToUserId:loggedIn._id}
+            ],
+        }).select("fromUserId ToUserId");
+      
+
+
+        const hideUserFromFeed=new Set();
+        Connection.forEach( (req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.ToUserId.toString());
+        });
+
+
+        const user=await User.find({
+            $and:[
+                {_id:{ $nin: Array.from(hideUserFromFeed)}},
+                {_id:{ $ne:loggedIn._id}}
+            ]
+        }).select(USER_SAFE).skip(skip).limit(limit);
+
+        console.log(hideUserFromFeed);
+        res.send(user);
+
+    }
+    catch(err){
+        res.status(400).send("ERROR:" + err.message);
+
+    }
+
+    const loggedIn=req.UserInfo;
+
+
+
 });
 
 
