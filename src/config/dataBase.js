23c6e8636b4ee4893ae8efc94c403ee17c579294
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2025-03-15 16:26:07 +0530

    FixBug:DB

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  try {
+
+  try{
     if (!process.env.DB_CONNECTION_SECRET) {
       throw new Error("❌ Missing DB_CONNECTION_SECRET in environment variables!");
     }
+    await mongoose.connect(
+  
+      process.env.DB_CONNECTION_SECRET
+     );
+     console.log("DB Connected");
 
-    await mongoose.connect(process.env.DB_CONNECTION_SECRET, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("✅ Database Connection established...");
-  } catch (error) {
-    console.error("❌ Database Connection Failed:", error.message);
-    process.exit(1); // Stop the process if DB connection fails
   }
+  catch(err){
+    console.error("❌ Database Connection Failed:", error.message);
+
+  }
+   
+   
+ 
 };
 
 module.exports = connectDb;
